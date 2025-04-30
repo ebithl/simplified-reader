d3e67a0927ee1e68d6da6f514c5fd583003fe5f8
Ebith Lawrence <ebith.lawrence@gmail.com>
2025-04-30 17:52:16 +0530

    Fix: removed the client side references from code

--- server/server.js
+++ server/server.js
@@ -22,10 +22,10 @@ app.use("/api/simplify/", simplifyRoutes);
 //app.use("/api/upload-pdf-pages", upload.single("pdf"), uploadRoutes);
 app.use("/api/upload-pdf-pages/", uploadRoutes);
 
-// Serve frontend build
-app.use(express.static(path.join(__dirname, "../client/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
+//// Serve frontend build
+//app.use(express.static(path.join(__dirname, "../client/dist")));
+//app.get("*", (req, res) => {
+//  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+//});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
