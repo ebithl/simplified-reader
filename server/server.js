10d9b2121473c2bc7a1012f5cfa259d10903c002
Ebith Lawrence <ebith.lawrence@gmail.com>
2025-04-30 19:52:10 +0530

    Fix: server side allow origin cors error

--- server/server.js
+++ server/server.js
@@ -16,11 +16,13 @@ const __dirname = path.dirname(__filename);
 
 const upload = multer({ dest: "uploads/" });
 
+
+app.use(express.json());
+
 //app.use(cors());
 app.use(cors({
   origin: process.env.ALLOW_ORIGIN
 }));
-app.use(express.json());
 app.use("/api/simplify/", simplifyRoutes);
 //app.use("/api/upload-pdf-pages", upload.single("pdf"), uploadRoutes);
 app.use("/api/upload-pdf-pages/", uploadRoutes);
