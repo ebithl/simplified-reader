0317d8c676f8540564afde70252b11867aa0e444
Ebith Lawrence <ebith.lawrence@gmail.com>
2025-05-01 16:31:32 +0530

    Fix: allow origin has been corrected

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const upload = multer({ dest: "uploads/" });
 app.use(express.json());
 
 app.use(cors({
-  origin: process.env.ALLOW_ORIGIN
+  origin: "https://simplified-reader-client.onrender.com"
 }));
 app.use("/api/simplify", simplifyRoutes);
 app.use("/api/upload-pdf-pages", upload.single("pdf"), uploadRoutes);
